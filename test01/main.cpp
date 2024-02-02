c3dab22de5cd50efdf4c3fd945a43572fc223412
2022051615114WuBoyuan <2271368131@qq.com>
2024-02-02 22:49:08 +0800

    新增刷题

--- test01/main.cpp
+++ test01/main.cpp
@@ -6,6 +6,9 @@ using std::endl;
 using std::string;
 #include <algorithm>
 using std::sort;
+#include <iomanip>
+#include <set>
+using std::set;
 
 // 问题解决日志
 // int w; // 多少组数据
@@ -28,6 +31,51 @@ using std::sort;
 //            }
 //        }
 //        cout << cnt << endl;
+//    }
+//    return 0;
+//}
+
+
+// 过河卒
+// int n, m, x, y;
+// int arr[23][23]; // 棋盘
+// int cnt; // 路的条数
+// int dx[8] = { -1, -1, -2, -2, 1, 1, 2, 2 };
+// int dy[8] = { -2, 2, -1, 1, -2, 2, -1, 1 };
+// void dfs(int nx, int ny)
+//{
+//    if (nx == n && ny == m) {
+//        cnt++;
+//        return;
+//    }
+//    // 可以向右或者向下行走
+//    if (nx + 1 >= 0 && nx + 1 <= n && arr[nx + 1][ny] != -1) {
+//        arr[nx + 1][ny] = -1;
+//        dfs(nx + 1, ny);
+//        arr[nx + 1][ny] = 0;
+//    }
+//    if (ny + 1 >= 0 && ny + 1 <= m && arr[nx][ny + 1] != -1) {
+//        arr[nx][ny + 1] = -1;
+//        dfs(nx, ny + 1);
+//        arr[nx][ny + 1] = 0;
+//    }
+//}
+// int main()
+//{
+//    cin >> n >> m >> x >> y; // 输入终点和马的坐标
+//    // 初始化棋盘
+//    arr[x][y] = -1;
+//    for (int i = 0; i < 8; ++i) {
+//        int xx = x + dx[i];
+//        int yy = y + dy[i];
+//        if (xx <= n && xx >= 0 && yy <= m && yy >= 0) {
+//            arr[xx][yy] = -1; // 标记为不可走的点
+//        }
+//    }
+//    dfs(0, 0);
+//    cout << cnt << endl;
+//    return 0;
+//}     cout << cnt << endl;
 //    }
 //    return 0;
 //}
@@ -201,11 +249,150 @@ using std::sort;
 //}
 
 
+// 统计方形
+//int n, m;
+//unsigned long long cntz, cntc;
+//int main()
+//{
+//    cin >> n >> m;
+//    // 枚举矩形所有可能的长宽
+//    for (int i = 1; i <= n; i++) {
+//        for (int j = 1; j <= m; j++) {
+//            if (i != j) {
+//                int t = (n - i + 1) * (m - j + 1);
+//                cntc += t;
+//            }else{
+//                int t = (n - i + 1) * (m - i + 1);
+//                cntz += t;
+//            }
+//        }
+//    }
+//    cout << cntz << " " << cntc << endl;
+//    return 0;
+//}
+
+
+// 全排列问题
+// int n;
+// int visit[10] = { 0 };
+// int arr[10];
+// void dfs(int a, int l)
+//{
+//    if (l == n) {
+//        for (int i = 1; i <= n; i++) {
+//            cout << std::setw(5)  <<arr[i];
+//        }
+//        cout << endl;
+//        return;
+//    }
+//    for (int i = 1; i <= n; i++) {
+//        if (!visit[i]) {
+//            visit[i] = 1;
+//            arr[l + 1] = i;
+//            dfs(i, l + 1);
+//            visit[i] = 0;
+//        }
+//    }
+//}
+// int main()
+//{
+//    cin >> n;
+//    for (int i = 1; i <= n; i++) {
+//        visit[i] = 1;
+//        arr[1] = i;
+//        dfs(i, 1);
+//        visit[i] = 0;
+//    }
+//    return 0;
+//}
+
+
+// 数的计算(暴力搜索)
+//#include <bits/stdc++.h>
+//using namespace std;
+//int n, cnt = 1;
+//void dfs(int a)
+//{
+//    for (int i = 1; i <= a / 2; i++) {
+//        cnt++;
+//        dfs(i);
+//    }
+//}
+//int main()
+//{
+//    std::ios::sync_with_stdio(false);  //关闭同步流
+//    cin >> n;
+//    dfs(n);
+//    cout << cnt << endl;
+//    return 0;
+//}
 
 
 
+// 第十四届蓝桥杯省赛C++ B组第一题
+// 思路:暴力，但是不要写dfs,直接写循环
+// int num[] = { 5, 6, 8, 9, 1, 6, 1, 2, 4, 9, 1, 9, 8, 2, 3, 6, 4, 7, 7, 5, 9, 5, 0, 3, 8, 7,
+//    5, 8, 1, 5, 8, 6, 1, 8, 3, 0, 3, 7, 9, 2, 7, 0, 5, 8, 8, 5, 7, 0, 9, 9, 1, 9, 4,
+//    4, 6, 8, 6, 3, 3, 8, 5, 1, 6, 3, 4, 6, 7, 0, 7, 8, 2, 7, 6, 8, 9, 5, 6, 5, 6, 1,
+//    4, 0, 1, 0, 0, 9, 4, 8, 0, 9, 1, 2, 8, 5, 0, 2, 5, 3, 3 };
+// int cnt;
+// set<int> resSet;
+// int md[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+// int main()
+//{
+//    for (int i1 = 0; i1 < 100; ++i1) {
+//        if (num[i1] == 2) {
+//            for (int i2 = i1 + 1; i2 < 100; ++i2) {
+//                if (num[i2] == 0) {
+//                    for (int i3 = i2 + 1; i3 < 100; ++i3) {
+//                        if (num[i3] == 2) {
+//                            for (int i4 = i3 + 1; i4 < 100; ++i4) {
+//                                if (num[i4] == 3) {
+//                                    for (int i5 = i4 + 1; i5 < 100; ++i5) {
+//                                        if (num[i5] <= 1) {
+//                                            for (int i6 = i5 + 1; i6 < 100; ++i6) {
+//                                                if (num[i6] <= 9 && num[i6] >= 1 && num[i5] == 0 || num[i6] <= 2 && num[i5] == 1) {
+//                                                    int m = num[i5] * 10 + num[i6];
+//                                                    for (int i7 = i6 + 1; i7 < 100; ++i7) {
+//                                                        if (num[i7] <= 3) {
+//                                                            for (int i8 = i7 + 1; i8 < 100; ++i8) {
+//                                                                int day = num[i7] * 10 + num[i8];
+//                                                                if (day != 0 && day <= md[m]) {
+//                                                                    resSet.insert(m * 100 + day);
+//                                                                }
+//                                                            }
+//                                                        }
+//                                                    }
+//                                                }
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+////    for (const auto e : resSet) {
+////        cout << e << endl;
+////    }
+//    std::set<int>::iterator it;
+//    for (it = resSet.begin(); it != resSet.end(); ++it) {
+//        std::cout << *it << "    ";
+//    }
+//    cout <<"\n最终结果为: "<< resSet.size() << endl;
+//    return 0;
+//}
 
 
+// 第十四届蓝桥杯省赛C++ B组第三题
+int main()
+{
+
+    return 0;
+}
 
 
 
