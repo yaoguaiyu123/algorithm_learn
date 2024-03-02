400f1565d3cadabde5b0b52ccdbcd9b772ca433c
2022051615114WuBoyuan <2271368131@qq.com>
2024-03-02 16:09:51 +0800

    新增一题

--- test01/main.cpp
+++ test01/main.cpp
@@ -1194,6 +1194,47 @@ using std::max;
 
 
 
+// 带分数
+//int arr[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//int cnt = 0;
+//int n;
+//int func(int a,int b)
+//{
+//    int sum=0;
+//    for(int i=a;i<=b;i++)
+//    {
+//        sum=sum*10+arr[i];
+//    }
+//    return sum;
+//}
+//int main()
+//{
+//    cin >> n;
+//    // 枚举所有的全怕列的可能
+//    do {
+//        // 在里面枚举所有的可能的分割情况
+//        for(int i=0;i<9;i++)
+//        {
+//            int a=func(0,i);
+//            for(int j=i+1;j<9;j++)
+//            {
+//                int b=func(i+1,j);
+//                int c=func(j+1,8);
+//                if(n*c==a*c+b)
+//                {
+//                    cnt++;
+//                }
+//            }
+//        }
+//    } while (std::next_permutation(arr, arr + 9));
+//    cout << cnt << endl;
+//    return 0;
+//}
+
+
+
+
+
 
 
 
